2b7c5630976e00608616d3a39c19e930cd31fe73
أنا <ahmad9317@hotmail.com>
2017-04-13 08:10:57 +0300

    Ahmad - PatientReport

--- MHealth/PatientReportViewController.swift
+++ MHealth/PatientReportViewController.swift
@@ -12,12 +12,51 @@ class PatientReportViewController: UIViewController, NetworkCaller {
     
     var callerManager:DoctorFunctions? = nil
 
+    @IBOutlet weak var bloodPressureSegment: UISegmentedControl!
+    @IBOutlet weak var heartBeatSegment: UISegmentedControl!
+    @IBOutlet weak var sugarLevelSegment: UISegmentedControl!
+    
+    @IBAction func bloodPressureChange(sender: UISegmentedControl) {
+    }
+    
+    @IBAction func heartBeatRateChange(sender: UISegmentedControl) {
+    }
+    
+    
+    @IBAction func sugarLevelChange(sender: UISegmentedControl) {
+    }
+    
+    
+    @IBOutlet weak var patientComments: UITextView!
+    
+    @IBOutlet weak var painLocation: UITextField!
+    
+    @IBAction func feverChange(sender: UISwitch) {
+    }
+    
+    @IBAction func nauseousChange(sender: UISwitch) {
+    }
+    
+    @IBAction func headacheChange(sender: UISwitch) {
+    }
+    
+    @IBAction func dizzinessChange(sender: UISwitch) {
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         callerManager = DoctorFunctions.myInstance()
         callerManager?.callerReport = self
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,6 +65,45 @@ class PatientReportViewController: UIViewController, NetworkCaller {
     }
     
     @IBAction func sendPatientReport(sender: AnyObject) {
+        
+        
+        let patientDic:NSDictionary = NSUserDefaults.standardUserDefaults().valueForKey(Const.UserDefaultsKeys.loggedinUser) as! NSDictionary
+        
+        let patient:Patient = Patient()
+        patient.loadDictionary(patientDic);
+        
+        
+        let newReport:PatientReportDH = PatientReportDH()
+        
+        newReport.bloodPressure = ""//High low natural
+        newReport.comments = ""
+        newReport.coughing = "" //YES NO
+        newReport.dizziness = ""
+        newReport.drId = 0 //
+        newReport.drcomment = ""
+        newReport.fever = ""//YES NO
+        newReport.headache = "" //YES NO
+        newReport.heartbeatRate = ""//High low moderate
+        newReport.nauseous = ""//NO YES
+//        if painLocation.text == "" {
+//            newReport.pain = false
+//            newReport.painlocation = ""
+//        }else{
+//            newReport.pain = true
+//            newReport.painlocation = ""
+//        }
+        newReport.sugarLevel = "" //High Low Medium
+        newReport.patientId = patient.patientID
+        
+        let rDic:NSDictionary = newReport.toDictionary()
+        
+        let reportDic:NSMutableDictionary = rDic.mutableCopy() as! NSMutableDictionary
+        reportDic.removeObjectForKey("name")
+        reportDic.removeObjectForKey("timestamp")
+        reportDic.removeObjectForKey("reportId")
+        
+        
+        
         
     }
 
